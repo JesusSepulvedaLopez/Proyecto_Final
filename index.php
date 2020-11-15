9ad63029049d222ffe8937ff52e4a746f650ec07
Jesus Sepulveda <jese_18@alu.uabcs.mx>
2020-11-14 22:47:37 -0700

    Avance de proyecto - vista de login

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./assets/css/app.css?v=0.0" media="all">
+    <title>
+        Free Movies
+    </title>
+</head>
+
+<body>
+    <div class="login_container">
+        <!-- Aqui empieza la barra de login de la pagina -->
+        <div class="login_bar">
+            <div class="logo_top">
+                Logo Pagina
+            </div>
+            <a href="login.php">
+                <div class="login_button1">
+                    Iniciar Sesion
+                </div>
+            </a>
+        </div>
+        <!-- Aqui termina la barra de login de la pagina -->
+
+        <!-- Aqui empieza el contenido del login de la pagina -->
+        <div class="content_login">
+            <h1>Encuentra todas tus <br> peliculas favoritas</h1>
+            <h3>Registrate e inicia tu prueba gratuita de 30 dias</h3>
+            <a href="">
+                <div class="register_button">
+                    Registrarse
+                </div>
+            </a>
+        </div>
+        <!-- Aqui termia el contenido del login de la pagina -->
+    </div>
+</body>
+
+</html>
